187cdec6366dca0004a64287bcc68358d2ab3464
Daniela Capponi <capponi.daniela@gmail.com>
2020-02-25 09:24:08 -0300

    syntaxis fix

--- js/toHackerSpeak.js
+++ js/toHackerSpeak.js
@@ -11,19 +11,10 @@
  */
 
 const toHackerSpeak = text => {
-    // bucle para recorrer
-    // for (let i = 0; i < text.length; i++) {
-    //     text = text.split(" ");
-    //     if (text[i] === "a") {
-    text = text.replace(/[a]/gi, "4"); // con replace() no haría falta el for, pero como no lo logré hacer sin replace(), lo dejo comentado para resolverlo
-    // } else if (text[i] === "e") {
+    text = text.replace(/[a]/gi, "4");
     text = text.replace(/[e]/gi, "3");
-    // } else if (text[i] === "i") {
     text = text.replace(/[i]/gi, "1");
-    // } else if (text[i] === "o") {
     text = text.replace(/[o]/gi, "0");
-    // } else if (text[i] === "s") {
     text = text.replace(/[s]/gi, "5");
-    // }
     return String(text);
 }
